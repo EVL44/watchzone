3fd000f6f6d446df7eaad4608108763424a3702d
EVL44 <spoope12345@gmail.com>
2025-07-18 17:59:26 +0100

    1.3.1

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import prisma from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic';
 const jwtSecret = process.env.JWT_SECRET || 'your-default-secret';
 
 export async function POST(request) {
